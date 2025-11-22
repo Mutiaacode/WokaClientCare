1659d103f38aa8124790582aac817004e2ed8d2b
Mutiaacode <149048644+Mutiaacode@users.noreply.github.com>
2025-11-22 12:02:57 +0700

    add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('admin.product.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('admin.product.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama_produk' => 'required|max:255',
+            'deskripsi' => 'nullable|string',
+            'harga_default' => 'nullable|numeric|min:0',
+        ]);
+
+        Product::create($validated);
+
+        return redirect()->route('admin.product.index')->with('sukses', 'Produk berhasil ditambahkan!');
+    }
+
+    public function edit($id)
+    {
+        return view('admin.product.edit', [
+            'product' => Product::findOrFail($id)
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $validated = $request->validate([
+            'nama_produk' => 'required|max:255',
+            'deskripsi' => 'nullable|string',
+            'harga_default' => 'nullable|numeric|min:0',
+        ]);
+
+        $product->update($validated);
+
+        return redirect()->route('admin.product.index')->with('sukses', 'Produk berhasil diperbarui!');
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+
+        return redirect()->route('admin.product.index')->with('sukses', 'Produk berhasil dihapus!');
+    }
+}
