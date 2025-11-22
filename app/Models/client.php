978c69976b1ca46577b5aea1f3f256a753d62804
Mutiaacode <149048644+Mutiaacode@users.noreply.github.com>
2025-11-22 10:12:16 +0700

    relasi model client ke user

--- app/Models/client.php
+++ app/Models/client.php
@@ -3,13 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class client extends Model
+class Client extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'user_id',
-        'nama_usaha',
-        'nomor_hp',
-        'alamat',
+        'user_id', 'nama_usaha', 'nomor_hp', 'alamat'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+  
 }
