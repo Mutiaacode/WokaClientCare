f38bc591a512f99ea2d951b4d0ebc9b589112d8b
Mutiaacode <149048644+Mutiaacode@users.noreply.github.com>
2025-11-22 17:32:26 +0700

    add tombol logout

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,14 +1,13 @@
 <aside class="left-sidebar" data-sidebarbg="skin6">
     <div class="scroll-sidebar" data-simplebar>
 
-        <div class="py-4 mt-3 ">
+        <div class="py-4 mt-3 text-center">
             <img src="{{ asset('assets/images/woka1.png') }}" alt="Woka Care Logo" style="width: 140px;">
         </div>
 
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
 
-                {{-- ========== ADMIN ========== --}}
                 @if (auth()->user()->role === 'admin')
                     <li class="sidebar-item">
                         <a class="sidebar-link" href="{{ route('admin.dashboard') }}">
@@ -24,7 +23,7 @@
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.clients.*') ? 'active' : '' }}">
                         <a class="sidebar-link" href="{{ route('admin.clients.index') }}">
                             <i class="ti ti-user"></i>
                             <span class="hide-menu">Clients</span>
@@ -82,103 +81,20 @@
                 @endif
 
 
+                {{-- Tambahkan di paling bawah --}}
+                <hr class="my-3">
 
-                {{-- ========== STAFF ========== --}}
-                @if (auth()->user()->role === 'staff')
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-layout-dashboard"></i>
-                            <span class="hide-menu">Dashboard</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-ticket"></i>
-                            <span class="hide-menu">Tickets</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-history"></i>
-                            <span class="hide-menu">Ticket Logs</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-user"></i>
-                            <span class="hide-menu">Clients</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-file-text"></i>
-                            <span class="hide-menu">Contracts</span>
-                        </a>
-                    </li>
-                @endif
-
-
-
-                {{-- ========== TEKNISI ========== --}}
-                @if (auth()->user()->role === 'teknisi')
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-layout-dashboard"></i>
-                            <span class="hide-menu">Dashboard</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-tools"></i>
-                            <span class="hide-menu">Maintenance</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-ticket"></i>
-                            <span class="hide-menu">Tickets</span>
-                        </a>
-                    </li>
-                @endif
-
-
-
-                {{-- ========== CLIENT ========== --}}
-                @if (auth()->user()->role === 'client')
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-layout-dashboard"></i>
-                            <span class="hide-menu">Dashboard</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-ticket"></i>
-                            <span class="hide-menu">Tickets</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-file-text"></i>
-                            <span class="hide-menu">Contracts</span>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-item">
-                        <a class="sidebar-link" href="#">
-                            <i class="ti ti-receipt"></i>
-                            <span class="hide-menu">Invoices</span>
-                        </a>
-                    </li>
-                @endif
+                <li class="sidebar-item mt-auto">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" 
+                                class="sidebar-link btn w-100 text-start d-flex align-items-center px-3 border-0 bg-transparent"
+                                style="padding-left: 14px;">
+                            <i class="ti ti-logout me-2"></i>
+                            <span class="hide-menu">Logout</span>
+                        </button>
+                    </form>
+                </li>
 
             </ul>
         </nav>
