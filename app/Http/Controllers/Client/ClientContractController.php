76c8728500937dffff4c7bf5a832297d962f1634
khansasukamieayam <khansabintari12@gmail.com>
2025-11-24 09:14:38 +0700

    add client views & controller

---
+++ app/Http/Controllers/Client/ClientContractController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contract;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ClientContractController extends Controller
+{
+    public function index()
+    {
+        $client = Auth::user()->client;
+
+        // jika tidak ada relasi client->contracts(), pake cara universal ini
+        $contracts = Contract::where('client_id', $client->id)->get();
+
+        return view('client.contract.index', compact('contracts'));
+    }
+
+     public function show($id)
+    {
+        $contract = Contract::with(['client', 'product'])->findOrFail($id);
+        return view('client.contract.show', compact('contract'));
+    }
+
+}
