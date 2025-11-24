70ca5e8473c5a7860c44e8275b60465d6ec7c262
Mutiaacode <149048644+Mutiaacode@users.noreply.github.com>
2025-11-24 10:06:03 +0700

    add route ticket

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,12 @@ use App\Http\Controllers\Admin\AdminClientController;
 use App\Http\Controllers\Admin\AdminProductController;
 use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\Admin\AdminContractController;
+use App\Http\Controllers\Admin\AdminTicketController;
+
+
 use App\Http\Controllers\Client\ClientContractController;
 use App\Http\Controllers\Client\ClientDashboardController;
+
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -37,6 +41,8 @@ Route::middleware(['auth', 'role:admin'])
         Route::resource('product', AdminProductController::class);
         Route::resource('users', AdminUserController::class);
         Route::resource('contract', AdminContractController::class);
+        Route::resource('ticket', AdminTicketController::class);
+
     });
 
 Route::middleware(['auth', 'role:client'])
@@ -46,9 +52,7 @@ Route::middleware(['auth', 'role:client'])
         Route::get('/dashboard', [ClientDashboardController::class, 'index'])
             ->name('dashboard');
         Route::get('/contract', [ClientContractController::class, 'index'])
-            ->name('contract.index');     
+            ->name('contract.index');
         Route::get('/contract/{id}', [ClientContractController::class, 'show'])
-            ->name('contract.show');       
+            ->name('contract.show');
     });
-
-
